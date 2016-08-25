bd2ab70ccb4376e683622b0b633d5a4555743e2f
Misikir <misikirmehari@gmail.com>
2016-08-25 13:50:52 -0600

    Fixed news_path and controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,7 +2,7 @@ require 'searchbing'
 class NewsController < ApplicationController
 
   def index
-    @news_feed = Bing.new('jmKxP+B6VBhVca/1t+dRz8qv6ySvNSrykDmRYE/2qN4',10,'News' 'Rails')
+    @news_feed = Bing.new('jmKxP+B6VBhVca/1t+dRz8qv6ySvNSrykDmRYE/2qN4',10,'News' )
     @news_results = @news_feed.search('Ruby on rails')
   end
 end
